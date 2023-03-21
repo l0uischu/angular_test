177f2606f16813b08d7c5161a5b444bd5be1aeeb
l0uischu <121117462+l0uischu@users.noreply.github.com>
2023-03-21 23:22:49 +0800

    update redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroFormComponent } from "./hero-form/hero-form.component";
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'form', component: HeroFormComponent },
+  { path: "", redirectTo: "", pathMatch: "full" },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "detail/:id", component: HeroDetailComponent },
+  { path: "heroes", component: HeroesComponent },
+  { path: "form", component: HeroFormComponent },
 ];
 
 @NgModule({
