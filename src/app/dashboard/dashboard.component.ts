54f6bec650fb48ff7873667d3cf8c9e1297c120c
l0uischu <121117462+l0uischu@users.noreply.github.com>
2024-08-29 06:37:48 +0000

    pill

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,11 +9,45 @@ import { HeroService } from '../hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+  totalNumber: number = 0;
+  numberOfRow: number = 0;
+  numberOfPill: number = 0;
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
    // this.getHeroes();
+  }
+  
+  getTriangleNumber(row: number) {
+    return (row * (row + 1)) / 2
+  }
+
+  getNumberOfRow(totalCount: number) {
+    return (Math.sqrt(8 * totalCount + 1) - 1) / 2
+  }
+
+  setTotalNumber() {
+    this.totalNumber = this.getTriangleNumber(this.numberOfRow) + this.numberOfPill
+  }
+
+  onNumberOfPillChange() {
+    this.setTotalNumber();
+  }
+
+  onTotalNumberChange() {
+    this.numberOfRow = this.getNumberOfRow(this.totalNumber)
+    if (!Number.isInteger(this.numberOfRow)) {
+      this.numberOfRow = Math.floor(this.numberOfRow)
+      this.numberOfPill = this.totalNumber - this.getTriangleNumber(this.numberOfRow)
+      return
+    } else {
+      return
+    }
+  }
+
+  onNumberOfRowChange() {
+    this.setTotalNumber();
   }
 
   getHeroes(): void {
